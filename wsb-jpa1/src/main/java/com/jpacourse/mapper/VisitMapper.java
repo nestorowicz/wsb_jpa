38ee0f6f817dfebcc20dce195e9d88f0813993d0
adul <aleksander.dul.77@gmail.com>
2025-03-22 15:47:40 +0100

    implement: lab 2 task 2

--- wsb-jpa1/src/main/java/com/jpacourse/mapper/VisitMapper.java
+++ wsb-jpa1/src/main/java/com/jpacourse/mapper/VisitMapper.java
@@ -1,6 +1,7 @@
 package com.jpacourse.mapper;
 
 import com.jpacourse.dto.VisitTO;
+import com.jpacourse.persistance.entity.MedicalTreatmentEntity;
 import com.jpacourse.persistance.entity.VisitEntity;
 
 public final class VisitMapper {
@@ -9,11 +10,13 @@ public final class VisitMapper {
         if (visitEntity == null) {
             return null;
         }
+
         final VisitTO visitTO = new VisitTO();
         visitTO.setTime(visitEntity.getTime());
-//        visitTO.setDoctorFirstName(visitEntity.getDoctor().getFirstName());
-//        visitTO.setDoctorLastName(visitEntity.getDoctor().getLastName());
-//        visitTO.setTreatmentTypes(visitEntity.getMedicalTreatments().stream().map(MedicalTreatmentEntity::getType).toList());
+        visitTO.setDoctorFirstName(visitEntity.getDoctor().getFirstName());
+        visitTO.setDoctorLastName(visitEntity.getDoctor().getLastName());
+        visitTO.setTreatmentTypes(visitEntity.getMedicalTreatments().stream().map(MedicalTreatmentEntity::getType).toList());
+
         return visitTO;
     }
 }
