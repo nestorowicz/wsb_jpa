59df3b5e20011f3a4ec930665e81fa5aed77efe3
Michal Nestorowicz <michal.nestorowicz@gmail.com>
2025-03-30 20:56:32 +0200

    feat: optimistic locking on Address

--- wsb-jpa1/src/test/resources/data/addresses.sql
+++ wsb-jpa1/src/test/resources/data/addresses.sql
@@ -1,8 +1,8 @@
-INSERT INTO ADDRESS (ID, ADDRESS_LINE1, ADDRESS_LINE2, CITY, POSTAL_CODE)
-VALUES (901, '250 Hospital Drive', 'Building A', 'New York', '10001'),
-       (902, '123 Main St', 'Apt 4B', 'Boston', '02108'),
-       (903, '87 Parkway Ave', 'Floor 3', 'Miami', '33101'),
-       (904, '789 Oak Road', NULL, 'Chicago', '60611'),
-       (905, '456 Pine Avenue', 'Suite 300', 'Seattle', '98101'),
-       (909, '456 Pine Avenue', 'Suite 300', 'Seattle', '98101'),
-       (910, '456 Pine Avenue', 'Suite 300', 'Seattle', '98101');
+INSERT INTO ADDRESS (ID, ADDRESS_LINE1, ADDRESS_LINE2, CITY, POSTAL_CODE, VERSION)
+VALUES (901, '250 Hospital Drive', 'Building A', 'New York', '10001', 1),
+       (902, '123 Main St', 'Apt 4B', 'Boston', '02108', 1),
+       (903, '87 Parkway Ave', 'Floor 3', 'Miami', '33101', 1),
+       (904, '789 Oak Road', NULL, 'Chicago', '60611', 1),
+       (905, '456 Pine Avenue', 'Suite 300', 'Seattle', '98101', 1),
+       (909, '456 Pine Avenue', 'Suite 300', 'Seattle', '98101', 1),
+       (910, '456 Pine Avenue', 'Suite 300', 'Seattle', '98101', 1);
